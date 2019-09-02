5e08639509b62da55bdd23cc8c5bf630d706d6dd
Ross <skyrossm@gmail.com>
2019-09-02 01:21:21 -0700

    aaaaaaaaaaaaaa

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ $(function() {
 	var currentMarker;
 	
 	var assetsUrl = function() {
-		return window.location.hostname == 'http://skyrossm.github.io/np-gangmap/';
+		return window.location.hostname = http://gta5-map.github.io/;
 	};
 
 	Handlebars.registerHelper('assetsUrl', assetsUrl);
